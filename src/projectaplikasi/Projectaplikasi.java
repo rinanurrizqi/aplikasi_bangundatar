439dfa5180ccb66f4a7d91cc29a88ea5fa12c78d
rinanurrizqi <rinanurrizqi@gmail.com>
2024-05-22 19:04:14 +0700

    bangun datar

---
+++ src/projectaplikasi/Projectaplikasi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectaplikasi;
+
+/**
+ *
+ * @author UsEr
+ */
+public class Projectaplikasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new home().setVisible(true);
+    }
+    
+}
